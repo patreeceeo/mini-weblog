2ab2fee489dfad8326d1fc64ac21854706a5f04f
Patrick Canfield <patrickcandoit@gmail.com>
2015-03-21 21:50:28 -0400

    add index view, serve static files in development

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,42 @@
-from flask import Flask
+import os
+from markdown import markdown
+from flask import Flask, send_file
 from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
+class Post(object):
+
+    @staticmethod
+    def path():
+        return os.path.join(os.getcwd(), "static", "posts")
+
+    @staticmethod
+    def path_for(filename):
+        return os.path.join(Post.path(), filename)
+
+    def __init__(self, name):
+        self.title = name
+        self.filepath = self.path_for(name)
+        self.url = '/post/' + name + '.html'
+        self.file = open(self.filepath)
+        raw_content = self.file.read()
+        self.content = markdown(raw_content)
+
+@app.route('/static/<subdir>/<filename>')
+def get_static_file(subdir, filename):
+    return send_file(os.path.join(os.getcwd(), 'static', subdir, filename))
+
 @app.route("/")
-def hello():
-    return "Hello, world! (I'm using Flask!)"
+def index():
+    posts = [ Post(os.path.basename(filename)) for filename in os.listdir(Post.path()) if os.path.isfile(Post.path_for(filename)) ]
+    return render_template('index.html', posts=posts)
 
 @app.route("/post/<basename>.html")
 def get_formatted_post(basename):
-    import os
-    from markdown import markdown
-    markdown_content = open("{cwd}/static/posts/{basename}.txt".format(cwd=os.getcwd(), basename=basename)).read()
-    return render_template('post.html', title=basename, content=markdown(markdown_content))
+    post = Post(basename)
+    return render_template('post.html', title=post.title, content=post.content)
+
 
 # if not app.debug:
 #    import logging
