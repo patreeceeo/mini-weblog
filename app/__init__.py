52cb5fbd76ac6a14147e5ad8279f09b27cc42e32
Patrick Canfield <patrickcandoit@gmail.com>
2015-04-01 16:09:26 -1000

    use separate dir and index.txt file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # encoding=utf8  
 from __future__ import unicode_literals
 import os
+import pyth
 from markdown import markdown
 from flask import Flask, send_file
 from flask import render_template
@@ -11,15 +12,15 @@ class Post(object):
 
     @staticmethod
     def path():
-        return os.path.join(os.getcwd(), "static", "posts")
+        return pyth.unix("~/weblog-data/")
 
     @staticmethod
     def path_for(filename):
-        return os.path.join(Post.path(), filename)
+        return os.path.join(Post.path(), filename.strip())
 
-    @staticmethod
-    def is_post_file(filename):
-        return os.path.isfile(Post.path_for(filename)) and filename[0] != "." and filename.split(".")[1] == "txt"
+    # @staticmethod
+    # def is_post_file(filename):
+    #     return os.path.isfile(Post.path_for(filename)) and filename[0] != "." and filename.split(".")[1] == "txt"
 
     def __init__(self, filename):
         without_extension = filename.split(".")[0]
@@ -40,7 +41,7 @@ def get_static_file(subdir, filename):
 
 @app.route("/")
 def index():
-    posts = [ Post(filename) for filename in os.listdir(Post.path()) if Post.is_post_file(filename) ]
+    posts = [ Post(filename) for filename in open(pyth.unix("~/weblog-data/index.txt")).readlines() ]
     return render_template('index.html', posts=posts)
 
 @app.route("/post/<slug>.html")
